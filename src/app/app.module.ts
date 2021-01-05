700420a1f1b1b0f8dbd79d77cb85539e36db16f2
Alan França <alan.franca@AvilaSolucoes.intranet>
2021-01-05 17:39:48 -0300

    starting project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// import {  } from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { AuthPageComponent } from './auth-page/auth-page.component';
+import { TopBarComponent } from './components/top-bar/top-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthPageComponent,
+    TopBarComponent
   ],
   imports: [
     BrowserModule
