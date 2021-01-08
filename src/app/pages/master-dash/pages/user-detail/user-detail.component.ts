77236e1e89299db04d5b91545cadac11fbd846a3
alandev2 <alanworking1@gmail.com>
2021-01-08 13:38:43 -0300

    adding user photo getting of backend

--- src/app/pages/master-dash/pages/user-detail/user-detail.component.ts
+++ src/app/pages/master-dash/pages/user-detail/user-detail.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface IImage {
+  filename: string;
+}
+
 @Component({
   selector: 'app-user-detail',
   templateUrl: './user-detail.component.html',
   styleUrls: ['./user-detail.component.css']
 })
 export class UserDetailComponent implements OnInit {
-
-  constructor() { }
+  name: string;
+  username: string;
+  password: string;
+  image: IImage;
+  constructor() {
+    var user = JSON.parse(localStorage.getItem("user"));
+    this.name = user.name;
+    this.username = user.username;
+    this.image = user.image;
+  }
 
   ngOnInit(): void {
+  }
+  
+  onSubmit(event: Event) {
+
   }
 
 }
